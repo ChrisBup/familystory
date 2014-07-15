f736d5a6412ad1c301d69befc74658910b05f823
ChrisBup <christopherheuberger@gmail.com>
2014-07-14 20:35:34 -0400

    start ziggeo implementation

---
+++ public/javascripts/backbone/models/video.js
@@ -0,0 +1,26 @@
+var FamilyStory = FamilyStory || { Models: {}, Collections: {}, Views: {} };
+
+FamilyStory.Models.Video = Backbone.Model.extend({
+
+{
+    "grants": {
+        "read": {
+            "resources": [
+                "1234567890"
+            ]
+        },
+        "update": {
+            "resources": [
+                "1234567890"
+            ]
+        },
+        "destroy": {
+            "resources": [
+                "1234567890"
+            ]
+        }
+    }
+}
+
+});
+
